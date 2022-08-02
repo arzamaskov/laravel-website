3d67e55f94dd30fdad450af64aad246764b98288
Andrey Arzamaskov <arzamaskov@gmail.com>
2022-08-02 10:52:02 +0400

    fix: modify index controller: add getting articles

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $posts = Post::orderBy('created_at', 'desc')->limit(6)->get();
+
+        return view('welcome', compact('posts'));
     }
 }
