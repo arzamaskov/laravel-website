5f9dc0be5e897936a7851d602e1d7e09d090989b
Andrey Arzamaskov <arzamaskov@gmail.com>
2022-08-02 10:52:59 +0400

    feat: create PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::orderBy('created_at', 'desc')->paginate(3);
+
+        return view('posts.index', compact('posts'));
+    }
+
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+
+        return view('posts.show', compact('post'));
+    }
+}
